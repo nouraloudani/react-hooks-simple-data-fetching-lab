aef228fd38ceb37249f86cda9e44c4ae13372317
Noura <contact.nouraloudani@gmail.com>
2021-02-05 23:24:37 -0500

    commit tree c5a1d49ed1f0f26f7544be9c76f35e57b263cd33

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,14 @@
-// create your App component here
+import React from "react";
+import Dogs from './Dogs'
+
+function App() {
+
+    return(
+        <div>
+            <Dogs />
+        </div>
+    )
+  
+}
+
+export default App;
